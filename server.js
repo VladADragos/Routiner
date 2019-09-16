74a2c3265e19ce6392d104cfd1ba92b66b6e60e1
VladADragos <vlad6620@yahoo.com>
2019-09-16 19:29:33 +0200

    fixed typo

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/api/activities', activities);
 if (process.env.NODE_DEV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'cleint', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
 

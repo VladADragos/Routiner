e79dfe59835336eeda48ee8084612f1f2ada433f
VladADragos <vlad6620@yahoo.com>
2019-09-16 19:37:29 +0200

    fixed typo

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/api/auth', auth);
 app.use('/api/routines', routines);
 app.use('/api/activities', activities);
 
-if (process.env.NODE_DEV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
